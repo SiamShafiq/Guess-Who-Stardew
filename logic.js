c4aa451010c4b226caddf65ab08748d3f4d37928
Siam Shafiq <siam.shafiq@gmail.com>
2022-04-19 04:24:49 -0700

    Multiplayer Initial Commit
    -Implemented intro page with create + find game buttons
    -Upgraded filter buttons
    -Choose own character
    -Game code generation

--- logic.js
+++ logic.js
@@ -1,20 +1,35 @@
 var imageNodes = document.getElementsByClassName('portraits');
 addEventListeners(imageNodes);
+let gameState = 'pick';
+let playerChar = "";
 
 function addEventListeners(imageNodes){
     for (var i = 0; i < imageNodes.length; i++){
         
         imageNodes[i].addEventListener("click", function() {
-            if($(this).hasClass("dark")){
-                $(this).removeClass("dark");
-
-            }else if(!$(this).hasClass('dark')){
-                $(this).addClass("dark");
+            if(gameState != 'pick'){
+                if($(this).hasClass("dark")){
+                    $(this).removeClass("dark");
+    
+                }else if(!$(this).hasClass('dark')){
+                    $(this).addClass("dark");
+                }
+            }else{
+                console.log($(this).attr("src"));
+                console.log($(this).parent().closest('div').text().trim());
+                setPlayerChar($(this).attr("src"), $(this).parent().closest('div').text().trim());
             }
+            
         });
     }
 }
 
+function setPlayerChar(imgSrc, charName){
+    $("#playerAva").attr('src', imgSrc);
+    $("#acceptCharBtn").prop('disabled', false);
+
+    $("#selectedChar").text(charName);
+}
 
 let nightModeOn = false;
 $("#nightBtn").click(function() {
@@ -34,59 +49,111 @@ $("#nightBtn").click(function() {
     }
 });
 
+var filter_female = "";
+var filter_male = "";
+var filter_marry = "";
+
+function resetFilters(){
+    $(".male, .female, .marry").css("backgroundColor", "");
+}
+
+var filter = `${filter_female}${filter_male}${filter_marry}`;
+
+function updateFilters(){
+    filter = `${filter_female}${filter_male}${filter_marry}`;
+    console.log(filter);
+
+    resetFilters();
+    $(`${filter}`).css("backgroundColor", "rgba(255, 0, 0, 0.5)");
+}
+    
+
 let femaleToggle = false;
-$("#female").click(function() {
-    if(!femaleToggle){
-        $(".portraits").filter(".female").css("backgroundColor", "rgba(255, 0, 0, 0.5)");
-        femaleToggle = true;
-
-        $('#female').css("backgroundColor", "red");
-    }else{
-        $(".portraits").filter(".female").css("backgroundColor", "");
-        femaleToggle = false;
-
-        $('#female').css("backgroundColor", "rgba(255, 255, 255, 0.2)");
-    }
-});
-
 let maleToggle = false;
-$("#male").click(function() {
-    if(!maleToggle){
-        $(".portraits").filter(".male").css("backgroundColor", "rgba(255, 0, 0, 0.5)");
-        maleToggle = true;
-
-        $('#male').css("backgroundColor", "red");
-    }else{
-        $(".portraits").filter(".male").css("backgroundColor", "");
-        maleToggle = false;
-
-        $('#male').css("backgroundColor", "rgba(255, 255, 255, 0.2)");
-
-    }
-});
-
 let marryToggle = false;
-$("#marry").click(function() {
-    if(!marryToggle){
-        $(".portraits").filter(".marry").css("backgroundColor", "rgba(255, 0, 0, 0.5)");
-        marryToggle = true;
 
-        $('#marry').css("backgroundColor", "red");
 
+$("#female").click(function(){
+    if(femaleToggle){
+        filter_female = "";
+        femaleToggle = false;
+        $('#female').css("backgroundColor", "transparent");
     }else{
-        $(".portraits").filter(".marry").css("backgroundColor", "");
+        filter_female = ".female";
+        femaleToggle = true;
+        $('#female').css("backgroundColor", "red");
+    }
+
+    updateFilters();
+})
+
+
+$("#male").click(function(){
+    if(maleToggle){
+        filter_male = "";
+        maleToggle = false;
+        $('#male').css("backgroundColor", "transparent");
+    }else{
+        filter_male = ".male";
+        maleToggle = true;
+        $('#male').css("backgroundColor", "red");
+    }
+
+    updateFilters();
+})
+
+$("#marry").click(function(){
+    if(marryToggle){
+        filter_marry = "";
         marryToggle = false;
+        $('#marry').css("backgroundColor", "transparent");
+    }else{
+        filter_marry = ".marry";
+        marryToggle = true;
+        $('#marry').css("backgroundColor", "red");
+    }
 
-        $('#marry').css("backgroundColor", "rgba(255, 255, 255, 0.2)");
+    updateFilters();
+})
 
-        
+
+//////
+
+$('#gameIDInput').on('input', function() {
+    if($("#gameIDInput").val().length == 6){
+        $("#findGame").prop('disabled', false);
+    }else{
+        $("#findGame").prop('disabled', true);
     }
 });
 
+$("#createGame").click(function() {
+    window.location.href = 'index.html';
+});
+
+$("#acceptCharBtn").click(function() {
+    gameState = "play";
+});
+
+function generateGameCode(){
+    return Math.random().toString(36).slice(7).toUpperCase();
+}
+
+function setGameCode(){
+    $("#gameCode").text(generateGameCode());
+}
+
+$(document).ready(function() {
+    setGameCode();
+    // $(".modal").modal('show');
+});
 
 
-
-
+// $(".character").hover(function(){
+//     $(this).append($("<button>***</button>"));
+// }, function (){
+//     $(this).find("button").last().remove();
+// } )
 
 
 
